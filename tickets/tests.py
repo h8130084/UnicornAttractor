89cac46b94c4c445e052f9f4d62f1e9a81600eae
Ubuntu <ubuntu@ip-172-31-84-242.ec2.internal>
2019-12-04 20:26:44 +0000

    amended travis.yml file and a previosus example test

--- tickets/tests.py
+++ tickets/tests.py
@@ -2,10 +2,3 @@ from django.test import TestCase
 from .models import Bug
 
 # Create your tests here.
-
-class BugTests(TestCase):
-    """testing"""
-
-    def test_str(self):
-        test_name = Bug(name= 'A Bug')
-        self.assertEqual(str(test_name), 'A Bug')
