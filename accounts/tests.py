f1d3ed8d926285d57a323a1592a75b3538c1023d
Ubuntu <ubuntu@ip-172-31-84-242.ec2.internal>
2019-12-08 17:17:08 +0000

    added more tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import resolve, reverse
-from .views import logout
+from .views import logout, login, registration, user_profile
 
 # Create your tests here.
 class TestAccountsUrls(TestCase):
@@ -8,3 +8,17 @@ class TestAccountsUrls(TestCase):
     def test_logout_url(self):
         path = resolve(reverse('logout'))
         self.assertEqual(path.func, logout)
+        
+
+    def test_login_url(self):
+        path = resolve(reverse('login'))
+        self.assertEqual(path.func, login)
+
+    
+    def test_registration_url(self):
+        path = resolve(reverse('registration'))
+        self.assertEqual(path.func, registration)
+        
+    def test_user_profile_url(self):
+        path = resolve(reverse('user_profile'))
+        self.assertEqual(path.func, user_profile)
