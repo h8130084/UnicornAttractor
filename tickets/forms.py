80184b8d276e43d0653d49c70a0627e3676d7211
Ubuntu <ubuntu@ip-172-31-84-242.ec2.internal>
2019-12-17 20:55:18 +0000

    styled all pages of website and had to amend current forms to allow new forms with widgets to be styled correctly,  tidyd code up generally and  made all pages more stream lined and user friendly for better UX. Finally amended navbar using bootstrap to allow for a drop down / hamburger button on smaller screens

--- tickets/forms.py
+++ tickets/forms.py
@@ -5,13 +5,28 @@ class AddBugForm(forms.ModelForm):
     class Meta:
         model = Bug
         fields = ('name', 'description', 'bug_or_feature')
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'text-input', 'placeholder': 'Name'}),
+            'description': forms.Textarea(attrs={'class': 'text-box', 'placeholder': 'Enter your description'}),
+            'bug_or_feature': forms.Select(attrs={'class': 'select-input'})
+        }
         
 class EditBugForm(forms.ModelForm):
     class Meta:
         model = Bug
         fields = ('name', 'description', 'bug_or_feature')
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'text-input', 'placeholder': 'Name'}),
+            'description': forms.Textarea(attrs={'class': 'text-box', 'placeholder': 'Enter your description'}),
+            'bug_or_feature': forms.Select(attrs={'class': 'select-input'})
+        }
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body', )
+        labels = {'body': 'Comment'}
+        widgets = {
+            'body': forms.Textarea(attrs={'class': 'text-box', 'placeholder': 'Enter your comment'}),
+        }
+        
